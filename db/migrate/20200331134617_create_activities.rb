6f150e02627b71c8365cc5be85d0e2dc3e1b0eff
quentin de malherbe <quentdemalherbe@gmail.com>
2020-04-01 10:33:45 +0200

    init projet

---
+++ db/migrate/20200331134617_create_activities.rb
@@ -0,0 +1,16 @@
+class CreateActivities < ActiveRecord::Migration[5.2]
+  def change
+    create_table :activities do |t|
+      t.string :name
+      t.date :date
+      t.integer :level
+      t.string :lieu
+      t.text :comment
+      t.string :price
+      t.references :user, foreign_key: true
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 20 fake category...'
+20.times do
+  category = Category.new(
+    name:    Faker::Esport.game,
+    photo:   Faker::LoremPixel.image(size: "50x60", is_gray: false, category: 'sports')
+  )
+  category.save!
+end
+puts'Category finished'
+puts 'Creating 5 fake users...'
+5.times do
+  user = User.new(
+    pseudo: Faker::Internet.username,
+    first_name: Faker::Name.first_name ,
+    last_name: Faker::Name.last_name ,
+    phone: Faker::Number.leading_zero_number(digits: 10),
+    email: Faker::Internet.email,
+    encrypted_password: Faker::Internet.password(min_length: 8)
+  )
+  user.save!
+end
+
+puts'Users finished'
+
+puts 'Creating 10 fake activities...'
+10.times do
+  offset1= rand(Category.count)
+  offset2 = rand(User.count)
+  activity = Activity.new(
+    name: Faker::Name.first_name,
+    date: Faker::Date.between(from: Date.today, to: 1.month.from_now),
+    level: rand(1..5),
+    lieu: Faker::Address.full_address,
+    comment: Faker::Lorem.paragraphs(number: 1),
+    price: Faker::Number.decimal_part(digits: 2),
+    user_id: User.find(offset2),
+    category_id: Category.find(offset1)
+  )
+  activity.save!
+end
+puts'Activities finished'
+
+puts 'Finished!'
